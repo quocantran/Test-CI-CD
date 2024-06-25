0ef82c298ab2d385e48ab67167658f2b5f5ae498
unknown <quocan142536@gmail.com>
2024-06-25 15:07:32 +0700

    test ci/cd

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ const userRoute = require("./user.route");
 const routes = (app) => {
   app.use("/", userRoute);
   app.use("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the API" });
+    res.status(200).json({
+      message: "Tôi tên là pê thủ khoa",
+      description: "Cân all các cháu 2k6",
+      math: 10,
+      physics: 10,
+      chemistry: 10,
+    });
   });
 };
 
