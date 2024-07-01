8d69bc3369cfa50de7fb09cfb893ec3ccd300a92
quocantran <114346752+quocantran@users.noreply.github.com>
2024-07-01 18:13:11 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ const routes = (app) => {
   app.use("/", userRoute);
   app.use("/", (req, res) => {
     res.status(200).json({
-      message: "Tôi tên là pê thủ khoa",
-      description: "Cân all các cháu 2k6",
-      math: 10,
-      physics: 10,
-      chemistry: 10,
+      message: "test",
+      description: "CI-CD",
     });
   });
 };
